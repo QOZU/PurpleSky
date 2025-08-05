4490227597d73a80266fbff40e34bb345213f9fb
QOZU <64510789+QOZU@users.noreply.github.com>
2025-08-05 20:16:00 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/purplesky/main.go
@@ -0,0 +1,23 @@
+// cmd/purplesky/main.go
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"purplesky/internal/purplesky"
+)
+
+func main() {
+	verbose := flag.Bool("verbose", false, "Enable verbose logging")
+	input := flag.String("input", "", "Input file path")
+	output := flag.String("output", "", "Output file path")
+	flag.Parse()
+
+	app := purplesky.NewApp(*verbose)
+	if err := app.Run(*input, *output); err != nil {
+		log.Printf("Error: %v", err)
+		os.Exit(1)
+	}
+}
